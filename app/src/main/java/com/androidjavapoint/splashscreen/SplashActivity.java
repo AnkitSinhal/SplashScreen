cc1aa8042d3590319f632b48df7387bec6dfab6d
AnkitSinhal <ankit.sinhal@gmail.com>
2017-01-15 02:00:37 +0530

    Initial commit

---
+++ app/src/main/java/com/androidjavapoint/splashscreen/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.androidjavapoint.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+
+        // close splash activity
+        finish();
+    }
+}
